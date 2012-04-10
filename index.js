8276231154f26f30305d8d5c4dd95c1fd1903ad8
Jesse Ditson <jesse.ditson@gmail.com>
2012-04-09 23:20:58 -0700

    adding width and height constraints and size detection to make autofit better. removing errant logging

--- index.js
+++ index.js
@@ -70,27 +70,31 @@ server.get(new RegExp("^/(.*)(?:.jpg)?$"), function(request, response, match) {
 
   var output = "/tmp/fuck-" + Math.floor(Math.random(10000000)*10000000) + '.jpg'
   download(match, output, function(){
-    var args = [
-      '-strokewidth','2',
-      '-stroke','black',
-      '-background','transparent',
-      '-fill','white',
-      '-gravity','center',
-      '-size','500x100',
-      "caption:"+unescape(msg),
-      output,
-      '+swap',
-      '-gravity','south',
-      '-composite',output
-    ]
-
-    im.convert(args, function(){
-      fs.readFile(output, function (err, data) {
-        if (err) throw err;
-        response.writeHead(200, {'Content-Type': 'image/jpeg' })
-        response.end(data)
+    im.identify(output,function(err,features){
+      var h = features.height < 100 ? features.height : 100
+        , w = features.width < 500 ? features.width : 500
+        , args = [
+            '-strokewidth','2',
+            '-stroke','black',
+            '-background','transparent',
+            '-fill','white',
+            '-gravity','center',
+            '-size',w+'x'+h,
+            "caption:"+unescape(msg),
+            output,
+            '+swap',
+            '-gravity','south',
+            '-size',w+'x',
+            '-composite',output
+          ];
+      im.convert(args, function(){
+        fs.readFile(output, function (err, data) {
+          if (err) throw err;
+          response.writeHead(200, {'Content-Type': 'image/jpeg' })
+          response.end(data)
+        });
       });
-    })
+    });
   })
 })
 
